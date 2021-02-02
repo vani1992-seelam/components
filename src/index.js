322c1c3c5db763071af0188dfd02cf199a9b845f
Bhaavani <vani.uideveloper@gmail.com>
2021-02-02 16:38:16 -0500

    Adding custom buttons by creating new component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CommentDetail from "./CommentDetail";
 import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
   return (
     <div className="ui container comments">
+      <ApprovalCard />
       <CommentDetail
         author="Sam"
         timeAgo="Today at 6:00PM"
