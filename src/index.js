5615eeee644abbfcaf13cf3b184ff13fd2ec41e5
Bhaavani <vani.uideveloper@gmail.com>
2021-02-02 16:18:34 -0500

    Using props from parent component to child

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import CommentDetail from "./CommentDetail";
+import faker from "faker";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="James" />
+      <CommentDetail
+        author="Sam"
+        timeAgo="Today at 6:00PM"
+        content="Nice Blog Post"
+        avatar={faker.image.image()}
+      />
+      <CommentDetail
+        author="Alex"
+        timeAgo="Today at 7:00PM"
+        content="I like Writing!"
+        avatar={faker.image.image()}
+      />
+      <CommentDetail
+        author="James"
+        timeAgo="Today at 8:00PM"
+        content="I like blogigng!!"
+        avatar={faker.image.image()}
+      />
     </div>
   );
 };
